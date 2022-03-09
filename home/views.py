e5f95d78b51fd9f2a378bf74aebbf3a219fa11ac
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-09 12:00:15 -0800

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,1 +1,10 @@
-a
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+def home(request):
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
