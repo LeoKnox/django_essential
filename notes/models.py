553d2052a67bb2007ea7be12df9c5f805087012d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-11 10:18:18 -0800

    Add files via upload

--- notes/models.py
+++ notes/models.py
@@ -1,1 +1,6 @@
-a
+from django.db import models
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
