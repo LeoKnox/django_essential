f8da71fe8c9ee9d550b84c920730c8d88feeea5c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-12 19:22:58 -0800

    Add files via upload

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
+    path('notes', views.NotesListview.as_view()),
+    path('notes/<int:pk>', views.DetailView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
-def list(request):
-     all_notes = Notes.objects.all()
-     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListview(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    
+class DetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
