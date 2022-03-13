af193b4ee43a6515c1e40e392204a9ea6d6f2c0a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-12 18:40:35 -0800

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -4,12 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_content = {'today':datetime.today()}
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
