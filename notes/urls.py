c2ede8610788eb2ea9ed0a47dc8b81570cecfe8b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-14 11:16:56 -0700

    Add files via upload

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('notes', views.NotesListview.as_view(), name="notes.list"),
-    path('notes/<int:pk>', views.DetailView.as_view()),
+    path('notes/<int:pk>', views.DetailView.as_view(), name="notes.detail"),
 ]
