41486c3e83bf68970b357d48c827add5d723cad5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-14 11:00:28 -0700

    Add files via upload

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListview.as_view()),
+    path('notes', views.NotesListview.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.DetailView.as_view()),
 ]
