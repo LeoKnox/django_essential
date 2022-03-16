c1659ad81772b8e7025a9f178a19a56f73f1073a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-16 10:28:22 -0700

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('notes', views.NotesListview.as_view(), name="notes.list"),
-    path('notes/<int:pk>', views.DetailView.as_view(), name="notes.detail"),
-    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
-]
