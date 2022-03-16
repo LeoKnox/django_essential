b0560a26f40b0d86f2e4543ad4ad5f59b4482297
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-16 12:02:24 -0700

    Add files via upload

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import Http404
 from .forms import NotesForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -11,16 +11,25 @@ class NotesDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = "/admin"
 
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 class NotesListview(LoginRequiredMixin, ListView):
     model = Notes
@@ -34,3 +43,4 @@ class NotesListview(LoginRequiredMixin, ListView):
 class DetailView(DetailView):
     model = Notes
     context_object_name = "note"
+    login_url = "/admin"
